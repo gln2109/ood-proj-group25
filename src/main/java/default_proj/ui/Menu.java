ccfb0ed56ea4850b3db29f11600a20a6203609e1
Joe <je2621@columbia.edu>
2025-11-21 15:18:53 -0500

    Modifed the files for our first meeting demonstration

--- src/main/java/default_proj/ui/Menu.java
+++ src/main/java/default_proj/ui/Menu.java
@@ -10,8 +10,8 @@ public class Menu {
         3 : Average Residence Market Value for Zip Code
         4 : Average Residence Livable Area for Zip Code
         5 : Residential Market Value per Capita for Zip Code
-        6 : Option 6
-        7 : Option 7
+        6 : (Data Sanity Check) Parking File Entries
+        7 : (Data Sanity Check) Property File Entries
         0 : Exit""";
 
     public static int promptSelection() {
@@ -48,5 +48,51 @@ public class Menu {
         return 0;
     }
 
+    public static int promptNumEntries(int max) {
+        Scanner sc = new Scanner(System.in);
+        int entries = 0;
+        System.out.print("How many entries: ");
+        String ln = sc.nextLine();
+        try {
+            entries = Integer.parseInt(ln);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number of entries\n");
+        }
+        if (entries > max)
+            entries = max;
+        if (entries < 0)
+            entries = 0;
+        return entries;
+    }
 
+    public static void printParkingEntries(ArrayList<String[]> parking_data) {
+        int num_entries = Menu.promptNumEntries(parking_data.size());
+        String[] entry, categories = {"date", "fine", "violation",
+                        "plate_id", "state", "ticket_number", "zip_code"};
+        System.out.println("\n================ Parking Entries ================\n");
+        for (int i = 0; i < num_entries; i++) {
+            entry = parking_data.get(i);
+            System.out.println("Entry # " + i + ": {");
+            for (int j = 0; j < categories.length; j++) {
+                System.out.println("\t" + categories[j] + ": " + entry[j]);
+            }
+            System.out.println("}\n");
+        }
+        System.out.println("=================================================\n");
+    }
+
+    public static void printPropertyEntries(ArrayList<String[]> property_data) {
+        int num_entries = promptNumEntries(property_data.size() - 1);
+        String[] entry, categories = property_data.get(0);
+        System.out.println("\n================ Property Entries ================\n");
+        for (int i = 1; i < num_entries + 1; i++) {
+            entry = property_data.get(i);
+            System.out.println("Entry # " + i + ": {");
+            for (int j = 0; j < categories.length; j++) {
+                System.out.println("\t" + categories[j] + ": " + entry[j]);
+            }
+            System.out.println("}\n");
+        }
+        System.out.println("==================================================\n");
+    }
 }
